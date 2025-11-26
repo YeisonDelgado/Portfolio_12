09039344a94750f4841cb27a05105643e01f9712
YEISON ESTIVEN DELGADO ORDOÑEZ <yeisondelgado@unicauca.edu.co>
2025-11-26 00:02:32 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

---
+++ src/components/fractal-sphere-loader.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import dynamic from 'next/dynamic';
+import { Skeleton } from '@/components/ui/skeleton';
+
+const FractalSphere = dynamic(
+  () => import('@/components/fractal-sphere').then((mod) => mod.FractalSphere),
+  {
+    ssr: false,
+    loading: () => (
+      <div className="absolute inset-0 flex items-center justify-center bg-transparent">
+        <Skeleton className="h-64 w-64 rounded-full" />
+      </div>
+    ),
+  }
+);
+
+export function FractalSphereLoader() {
+  return <FractalSphere />;
+}
