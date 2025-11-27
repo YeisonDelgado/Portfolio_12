19d285bdb87473abecadb6ecfb9d0bea99c53429
YEISON ESTIVEN DELGADO ORDOÑEZ <yeisondelgado@unicauca.edu.co>
2025-11-27 04:15:52 +0000

    yo lo veo igual, coemcemos por dejar todo el fondo de la paguina de un s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,8 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen w-full bg-background text-foreground">
       <main className="flex-grow">
-        <div className="relative w-full h-[50vh] bg-gradient-to-b from-[#000000] to-[#111111]">
+        <div className="relative w-full h-[50vh]">
           <FractalSphereLoader isEnergized={isEnergized} />
-          <div className="absolute inset-0 bg-black/40" />
         </div>
         <Intro isEnergized={isEnergized} setIsEnergized={setIsEnergized} />
         <div className="container mx-auto px-4 md:px-6">
